e1a2b0604fecbac73df927c4e188a3cb89f1c3fe
abebsko <seyibal@gmail.com>
2022-03-17 19:37:23 +0000

    updated login and create story page

---
+++ login.php
@@ -0,0 +1,58 @@
+<?php 
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+    <!-- Bootstrap CSS -->
+    <link href="assets/css/bootstrap.min.css" rel="stylesheet" />
+    <!-- Fonts -->
+    <link
+      href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"
+      rel="stylesheet"
+    />
+    <link
+      href="https://fonts.googleapis.com/css?family=Righteous"
+      rel="stylesheet"
+    />
+    <!-- manual styles -->
+    <link rel="stylesheet" href="assets/css/style.css">
+</head>
+<body class= "login-page">
+    <!--Login Container-->
+    <div class="container">
+        <div class= "row justify-content-center"> 
+<div class="card col-md-4 form-popup" id= "login">
+    <div class="text-center">
+        <div class="px-1 close"> <a href="index.html">&times;</a></div>
+              </div>
+            <div class="form mt-3">
+                <label class= "input-label" for="username"> <b>Username:</b></label>
+            <input class="input-box" type="text" placeholder= "Enter Email" name ="username" required> 
+            <label class= "input-label"for="password"> <b>Password:</b></label>
+            <input class="input-box" type="text" placeholder="Enter Password" name= "password"required>
+<button type= "submit" class="btn btn-login  my-4"> Login </button>
+                 </div>
+<span class= "sup"> Don't have an account? <a href="#">Sign Up </a></span>
+    </div>  
+    </div>
+     </div>
+<!--Login Container Ends-->
+    <!--  JavaScript
+    ================================================== -->
+     <script src="assets/js/jquery.min.js"></script>
+    <script
+      src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js"
+      integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb"
+      crossorigin="anonymous"
+    ></script>
+    <script src="assets/js/bootstrap.min.js"></script>
+    <script src="assets/js/ie10-viewport-bug-workaround.js"></script>
+  
+</body>
+</html>
---
+++ write.php
@@ -0,0 +1,79 @@
+<?php 
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Write</title>
+    <!-- Bootstrap CSS -->
+    <link href="assets/css/bootstrap.min.css" rel="stylesheet" />
+    <!-- Fonts -->
+    <link
+      href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"
+      rel="stylesheet"
+    />
+    <link
+      href="https://fonts.googleapis.com/css?family=Righteous"
+      rel="stylesheet"
+    />
+    <!-- manual styles -->
+    <link rel="stylesheet" href="assets/css/style.css">
+</head>
+<body class= "write-page">
+    <!--Login Container-->
+    <div class="container">
+        <div class= "row justify-content-center"> 
+<div class="card write-popup" id= "write">
+    <div class="text-center">
+        <div class="px-1 close"> <a class= "text-success"href="profile.html">&times;</a></div>
+            </div>
+            <div class="form mt-3">
+                <label class= "input-label" for="postTitle"> <b>Title:</b></label>
+            <input type="text" name ="titleBox" required> 
+             <label class= "input-label" for="postTitle"> <b>Category:</b></label>
+            <select class= "select-category">
+           <option value="None">Select Category</option>
+            <option value="Mus"> Museums & Castles</option>
+            <option value="Res">Restaurants</option>
+            <option value="Ent"> Entertainment</option>
+          </select>
+          <p class= "input-label">
+            Write Something: 
+           <div>
+<textarea id="story" name = "story" rows = "10" cols = "40"></textarea>
+          
+           </div> </p>
+           <p> <span class= "input-label"> Upload Image:</span>
+ <input type="file" id="postImg" name="postImg">
+           </p>
+           <p> <input type="submit" value="POST" class="btn btn-success" />
+        </p>
+                   </div>
+
+    </div>
+
+
+   
+  
+
+    
+    </div>
+     </div>
+<!--Login Container Ends-->
+    <!--  JavaScript
+    ================================================== -->
+     <script src="assets/js/jquery.min.js"></script>
+    <script
+      src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js"
+      integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb"
+      crossorigin="anonymous"
+    ></script>
+    <script src="assets/js/bootstrap.min.js"></script>
+    <script src="assets/js/ie10-viewport-bug-workaround.js"></script>
+  
+</body>
+</html>
